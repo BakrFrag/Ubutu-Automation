799d6bad50b6da5e35e880e3c031b3199afa024b
root <bakr.frag@gmail.com>
2020-03-25 10:30:52 +0200

    install client server for databases mysql , postgresql and sqlite

--- Custom_Ubuntu.sh
+++ Custom_Ubuntu.sh
@@ -1,4 +1,20 @@
+# Switch To Bash Sudo Priviliages
 sudo bash
+
+# Upadte And Applay Upgrade
 sudo apt-get update
 sudo apt-get dist-upgrade
+# install database client drivers
+
+# Postgresql
+sudo apt-get install libpq-dev
+
+# mysql
+sudo apt-get install mysqlclient-dev
+
+# Sqlite3
+
+sudo apt-get install libsqlite3-dev
+
+# Install Python3 Pip 
 sudo apt-get install python3-pip
